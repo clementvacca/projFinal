a28cb8f779421d4370410ce35072b5b8d4a8cd48
Your Name <you@example.com>
2019-02-19 12:24:22 +0100

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {routes} from './routes';
 import {FormsModule} from '@angular/forms';
+import { FormationComponent } from './rest/formation/formation/formation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormationComponent
   ],
   imports: [
     BrowserModule, FormsModule, RouterModule.forRoot(routes), HttpClientModule
