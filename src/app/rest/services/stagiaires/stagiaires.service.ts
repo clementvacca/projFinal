8ecea94f4916bf06b167826d95d8922b8e3eccd9
vacca <clement.vacca1@outlook.com>
2019-02-19 12:17:07 +0100

    component model pour stagiaires

--- src/app/rest/services/stagiaires/stagiaires.service.ts
+++ src/app/rest/services/stagiaires/stagiaires.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StagiairesService {
+  private headers: HttpHeaders;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+  }
+
+  public findAll(): Observable<any> {
+    return this.http.get<any>('http://localhost:8080/ajc/rest/stagiaires/', {headers: this.headers});
+  }
+
 }
